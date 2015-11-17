b7970f7714634dabd1fd22647bbdc120cc34bcd2
Chris Heathcote <chris.heathcote@digital.cabinet-office.gov.uk>
2015-11-17 12:47:33 +0000

    move to Flask-Scss

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
 import os
 from flask import Flask, render_template
 from flask.ext import assets
+from flask.ext.scss import Scss
 
 app = Flask(__name__)
+Scss(app, static_dir='static', asset_dir='assets/stylesheets')
 
 env = assets.Environment(app)
 
 # Tell flask-assets where to look for our sass files.
 env.load_path = [
-    os.path.join(os.path.dirname(__file__), 'sass')
+    os.path.join(os.path.dirname(__file__), 'stylesheets')
 ]
-
-
-
-
-env.register(
-    'css_all',
-    assets.Bundle(
-        'all.sass',
-        filters='sass',
-        output='css_all.css'
-    )
-)
 
 
 @app.route("/")
