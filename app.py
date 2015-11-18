816bfecbfdbfba0e8c09b61241a380d7dabb57fc
Chris Heathcote <chris.heathcote@digital.cabinet-office.gov.uk>
2015-11-18 11:57:50 +0000

    got sass compiling one import file

--- app.py
+++ app.py
@@ -1,28 +1,36 @@
 import os
 from flask import Flask, render_template
 from flask.ext import assets
+from flask.ext.assets import Environment, Bundle
 #from flask.ext.scss import Scss
 #from flask.ext.sass import sass
 from webassets.filter import get_filter
+
 
 app = Flask(__name__)
 #Scss(app, static_dir='static', asset_dir='assets/stylesheets')
 #sass(app, input_dir='assets/stylesheets', output_dir='static')
 
+app.config['ASSETS_DEBUG'] = True
+
 env = assets.Environment(app)
+
+env.config['sass_bin'] = '/usr/local/bin/sass'
 
 # Tell flask-assets where to look for our sass files.
 env.load_path = [
     os.path.join(os.path.dirname(__file__), 'assets/stylesheets'),
-    os.path.join(os.path.dirname(__file__), 'assets')
+    os.path.join(os.path.dirname(__file__), 'assets'),
+    os.path.join(os.path.dirname(__file__), 'assets/stylesheets/govuk_frontend_toolkit')
 ]
+
 
 scss = get_filter('scss', as_output=True)
 
 env.register(
     'css_all',
     assets.Bundle(
-        '*.scss',
+        'main.scss',
         filters='scss',
         output='static/css_all.css'
     )
